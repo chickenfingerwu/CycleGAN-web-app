f519aa00288e8bdc00dd7d535c94ddd09b0c0324
chickenfingerwu <swlpham@gmail.com>
2020-05-16 21:08:55 +0700

    fixed

--- CycleGAN/settings.py
+++ CycleGAN/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
